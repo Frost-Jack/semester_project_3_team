e795013f2db51635ee866940323465fc1f7dead5
FrostJack <trokhan.aleksandr@gmail.com>
2021-12-09 10:24:05 +0300

    several authors

--- src/main/java/com/example/accessingdata/User.java
+++ src/main/java/com/example/accessingdata/User.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.Base64;
-
+//Ну коммент появился.
 @Entity // This tells Hibernate to make a table out of this class
 @Table(name = "users_data",
 		uniqueConstraints = {@UniqueConstraint(columnNames = {"login"})}
