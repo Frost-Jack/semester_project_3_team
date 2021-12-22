61113111a4ba48c295b3b5f38e7d30d0a67cc066
GarnetAki <RomeNightingale@gmail.com>
2021-12-22 22:58:28 +0300

    new tests

--- src/test/java/com/example/accessingdata/AccessingDataApplicationTests.java
+++ src/test/java/com/example/accessingdata/AccessingDataApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.accessingdata;
 
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,8 +11,16 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 public class AccessingDataApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void TestSettingId() {
+		User logger = new User();
+		logger.setId(1);
+		logger.getId();
 	}
-
+	@Test
+	void TestSettingFirstName() {
+		User logger = new User();
+		logger.setFirstName("Y");
+		logger.getId();
+	}
 }
 
